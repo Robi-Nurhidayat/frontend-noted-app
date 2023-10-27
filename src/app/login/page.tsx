2d280441af7b583c19b5203c6d9c89d011e661ed
Robi Nurhidayat <robivanpersie290899@gmail.com>
2023-10-27 20:46:45 +0700

    login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,47 +1,22 @@
 import Image from "next/image";
 import Link from "next/link";
-import ImageRegister from "public/image-register.png";
+import ImageLogin from "public/image-login.jpg";
 
-const Register = () => {
+const Login = () => {
   return (
     <div className="w-screen h-screen bg-blue-400 flex">
       <div className=" flex-[0_0_564px]  h-full">
         <Image
-          src={ImageRegister}
+          src={ImageLogin}
           alt="image"
-          className="h-full object-cover"
+          className="w-full h-full object-cover"
         />
       </div>
       <div className="flex-1 flex flex-col items-center justify-center">
         <h1 className="text-[36px] font-[500] text-white mb-[40px]">
-          Daftar Sekarang!
+          Sign In to Your Account
         </h1>
         <form action="#">
-          <div className="flex flex-col mb-[18px]">
-            <label htmlFor="name" className="text-[20px] font-[500] text-white">
-              Name
-            </label>
-            <input
-              type="text"
-              name="name"
-              className="text-[16px] text-[#BFBFBF] py-4 pl-[30px] pr-[51px] rounded-full w-[400px] outline-none"
-              placeholder="Masukkan nama lengkap Anda "
-            />
-          </div>
-          <div className="flex flex-col mb-[18px]">
-            <label
-              htmlFor="username"
-              className="text-[20px] font-[500] text-white"
-            >
-              Username
-            </label>
-            <input
-              type="text"
-              name="username"
-              className="text-[16px] text-[#BFBFBF] py-4 pl-[30px] pr-[51px] rounded-full w-[400px] outline-none"
-              placeholder="Masukkan nama lengkap Anda "
-            />
-          </div>
           <div className="flex flex-col mb-[18px]">
             <label
               htmlFor="email"
@@ -75,18 +50,18 @@ const Register = () => {
               type="submit"
               className="text-[20px] font-[600] text-white rounded-full w-full bg-[#FF872E] p-[12px_149px_13px_150px]"
             >
-              Register
+              Login
             </button>
           </div>
         </form>
         <p className="mt-5 text-base text-white">
-          Sudah punya akun?{" "}
+          Don’t have account?
           <Link href={"/login"} className="text-[#FF872E] ml-1">
-            Sign In
+            Sign Up
           </Link>
         </p>
       </div>
     </div>
   );
 };
-export default Register;
+export default Login;
